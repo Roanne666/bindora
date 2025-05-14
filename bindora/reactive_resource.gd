9a15b2019f55bc8cbf3f8e8e85127d57e817018c
Roanne <a275313730@qq.com>
2025-05-14 20:00:00 +0800

    refactor(reactive_resource): move serialization logic from Bindora to ReactiveResource

--- bindora/reactive_resource.gd
+++ bindora/reactive_resource.gd
@@ -2,7 +2,36 @@
 class_name ReactiveResource extends Resource
 ## A resource that can be serialized and deserialized.
 
+const __FLAGS__ = PROPERTY_USAGE_SCRIPT_VARIABLE
+
 var __refs__: Dictionary[String, Variant.Type] = {}
+
+
+## Deserializes a [Dictionary] into a [ReactiveResource].
+static func reactive(_dict: Dictionary, _class) -> ReactiveResource:
+	var obj = _class.new()
+	for prop in obj.get_property_list():
+		if prop.usage & __FLAGS__ > 0:
+			var value = _dict.get(prop.name)
+			var obj_value = obj.get(prop.name)
+			if obj_value is Ref:
+				obj_value.value = value
+			else:
+				obj.set(prop.name, value)
+	return obj
+
+
+## Serialize a [ReactiveResource] into a [Dictionary].
+static func serialize(_obj: ReactiveResource) -> Dictionary:
+	var dict := {}
+	for prop in _obj.get_property_list():
+		if prop.usage & __FLAGS__ > 0:
+			var value = _obj.get(prop.name)
+			if value is Ref:
+				value = value.value
+			dict[prop.name] = value
+	return dict
+
 
 func _init() -> void:
 	__refs__ = {}
@@ -14,7 +43,7 @@ func _init() -> void:
 func _get_property_list() -> Array[Dictionary]:
 	var properties: Array[Dictionary] = []
 	for k in __refs__:
-		properties.append({"name": "_%s" %k, "type": __refs__[k], "usage": PROPERTY_USAGE_DEFAULT})
+		properties.append({"name": "_%s"%k, "type": __refs__[k], "usage": PROPERTY_USAGE_DEFAULT})
 	return properties
 
 func _set(property: StringName, value: Variant) -> bool:
@@ -40,7 +69,7 @@ func to_dictionary() -> Dictionary:
 	for prop in get_property_list():
 		if prop["name"] == "__refs__":
 			continue
-		if prop["usage"] & Bindora.FLAGS > 0:
+		if prop["usage"] & __FLAGS__ > 0:
 			var value = get(prop.name)
 			if value is Ref:
 				value = value.value
@@ -50,7 +79,7 @@ func to_dictionary() -> Dictionary:
 ## Update self from a dictionary.
 func from_dictionary(_dict: Dictionary) -> void:
 	for prop in get_property_list():
-		if prop["usage"] & Bindora.FLAGS > 0:
+		if prop["usage"] & __FLAGS__ > 0:
 			var variable = get(prop["name"])
 			if variable is Ref:
 				variable.value = _dict.get(prop["name"])
