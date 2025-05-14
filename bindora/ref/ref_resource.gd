70ed465d8e38460de03309ba9a3860c22847f40d
Roanne <a275313730@qq.com>
2025-05-14 20:44:17 +0800

    fix(ref_resource): fix export property

--- bindora/ref/ref_resource.gd
+++ bindora/ref/ref_resource.gd
@@ -4,13 +4,7 @@ class_name RefResource extends Ref
 var resource_type: String
 
 
-func _get_property_list() -> Array[Dictionary]:
-	return [
-		{
-			"name": "value",
-			"type": type,
-			"hint": PROPERTY_HINT_RESOURCE_TYPE,
-			"hint_string": resource_type,
-			"usage": PROPERTY_USAGE_DEFAULT
-		}
-	]
+func _validate_property(property: Dictionary):
+	if property["name"] == "value":
+		property["hint"] = PROPERTY_HINT_RESOURCE_TYPE
+		property["hint_string"] = resource_type
