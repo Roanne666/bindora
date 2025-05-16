4b1148a500e0f8d8f060d86cf97d0070a370f81a
Roanne <a275313730@qq.com>
2025-05-16 16:37:20 +0800

    feat(watcher): add methods to manage refs in multi_watcher

--- bindora/watcher/multi_watcher.gd
+++ bindora/watcher/multi_watcher.gd
@@ -7,7 +7,23 @@ func _init(_refs: Array[Ref], _callable: Callable) -> void:
 	refs = _refs
 	callable = _callable
 	for r in refs:
-		r._watchers.append(self)
+		r.add_watcher(self)
+	pass
+
+
+func add_ref(_ref: Ref) -> void:
+	if refs.has(_ref):
+		return
+	refs.append(_ref)
+	_ref.add_watcher(self)
+	pass
+
+
+func remove_ref(_ref: Ref) -> void:
+	if not refs.has(_ref):
+		return
+	refs.erase(_ref)
+	_ref.remove_watcher(self)
 	pass
 
 
