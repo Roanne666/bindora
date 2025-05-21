05c57b6b8455fa43fb93b4680abf389f61c1357f
Roanne <a275313730@qq.com>
2025-05-21 14:43:45 +0800

    refactor(list_binding): improve binding management with __bindings__ array

--- bindora/binding/single_ref_binding/list_binding.gd
+++ bindora/binding/single_ref_binding/list_binding.gd
@@ -15,10 +15,10 @@ var packed_scene: PackedScene
 ## Signature: func(instance: Node, data: Variant, index: int) -> void
 var callable: Callable
 
+var __bindings__: Array = []
 
-func _init(
-	_node: CanvasItem, _ref: RefArray, _packed_scene: PackedScene, _callable: Callable
-) -> void:
+
+func _init(_node: CanvasItem, _ref: RefArray, _packed_scene: PackedScene, _callable: Callable) -> void:
 	super (_node, _ref)
 	packed_scene = _packed_scene
 	callable = _callable
@@ -26,22 +26,35 @@ func _init(
 
 
 func _update(_diff: int, _arg) -> void:
-	# Handle incremental updates if we know the changed index
-	if typeof(_diff) == TYPE_INT and _diff > -1:
+	if _diff > -1:
+		# Handle incremental updates if we know the changed index
 		if ref.size() > node.get_child_count():
 			# Item was added - create and position new instance
 			var data = ref.value[_diff]
 			var instance = packed_scene.instantiate()
 			node.add_child(instance)
 			node.move_child(instance, _diff)
-			callable.call(instance, data, _diff)
+			__bindings__.insert(_diff, callable.call(instance, data, _diff))
 		elif ref.size() < node.get_child_count():
+			for b in __bindings__[_diff]:
+				if b is Binding:
+					b.destroy()
+			__bindings__.remove_at(_diff)
+
 			# Item was removed - clean up corresponding node
 			var c = node.get_child(_diff)
 			node.remove_child(c)
 			c.queue_free()
 	else:
+		# TODO: More efficient solution
 		# Full refresh needed - rebuild entire list
+		# Clear existing bindings
+		for b in __bindings__:
+			for bb in b:
+				if bb is Binding:
+					bb.destroy()
+		__bindings__.clear()
+
 		# Clear existing children
 		for c in node.get_children():
 			node.remove_child(c)
@@ -52,5 +65,5 @@ func _update(_diff: int, _arg) -> void:
 			var data = ref.value[i]
 			var instance = packed_scene.instantiate()
 			node.add_child(instance)
-			callable.call(instance, data, i)
+			__bindings__.insert(i, callable.call(instance, data, i))
 	pass
