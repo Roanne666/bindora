933525eb1a8df9dab5801e9667f97827d5d7c7ff
Roanne <a275313730@qq.com>
2025-05-23 16:35:59 +0800

    refactor(binding): rename `node` to `__node__` and improve disposal

--- bindora/binding/binding.gd
+++ bindora/binding/binding.gd
@@ -5,18 +5,18 @@ class_name Binding extends Object
 ## and UI elements or other nodes in the scene tree.
 
 ## The target node this binding is attached to.
-var node: Node
+var __node__: Node
 
 
 ## Initializes the binding with a target node
 func _init(_node: CanvasItem) -> void:
-	node = _node
-	node.tree_exiting.connect(_dispose)
+	__node__ = _node
+	__node__.tree_exiting.connect(_dispose)
 	pass
 
 
 func _on_ref_value_changed(_old_value, _new_value) -> void:
-	if node == null:
+	if __node__ == null:
 		_dispose()
 	else:
 		_update(_old_value, _new_value)
@@ -29,6 +29,6 @@ func _update(_old_value, _new_value) -> void:
 
 ## Cleans up the binding resources
 func _dispose() -> void:
-	node.tree_exiting.disconnect(_dispose)
-	node = null
+	__node__.tree_exiting.disconnect(_dispose)
+	call_deferred("free")
 	pass
